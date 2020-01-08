3d2716b8c2bb94e0a8f7d3246960d8dde0f1c866
luml <1520622848@qq.com>
2020-01-08 18:01:51 +0800

    sharp images

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: [
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
-const ImageBackground = styled('div')`
-  background-image: url('/images/zhang-kaiyv-beijing.jpg');
+const ImageBackground = styled(BackgroundImage)`
   background-position: top 20% center;
   background-size: cover;
   height: 50vh;
@@ -39,9 +39,20 @@ const TextBox = styled('div')`
 `;
 
 const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "zhang-kaiyv-beijing.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    // We don't do any sharp process for hero image yet
-    <ImageBackground>
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
       <TextBox>
         <h1>Frontend Masters + Gatsby &hearts;</h1>
         <p>
