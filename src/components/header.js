fe55a2eec2d2734d0144127143dcc4dc25e9dd84
luml <lumeiling@bonc.com.cn>
2020-10-05 15:01:58 +0800

    feat: update header

--- src/components/header.js
+++ src/components/header.js
@@ -33,7 +33,7 @@ const Header = () => (
     `}
   >
     <NavLink to="/" fontWeight="bold">
-      <span role="img" aria-label="dolphin">💠</span>To Be A Master
+      <span role="img" aria-label="dolphin">💠</span>Be in a process
     </NavLink>
     <nav
       css={css`
